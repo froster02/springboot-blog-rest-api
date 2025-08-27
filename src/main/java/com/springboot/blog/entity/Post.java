f5fdc115fb3ece80f1f5741edbc158bfdf4b7cb6
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-08-27 22:31:12 +0530

    feat: add Category entity and establish relationship with Post

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -59,4 +59,8 @@ public class Post {
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     private Set<Comment> comments = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
