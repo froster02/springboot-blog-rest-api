32401ef9bec09efb8608106e69c39574d1e5a24a
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-08-28 21:00:35 +0530

    feat: add Get Category API to retrieve category details by ID

--- src/main/java/com/springboot/blog/service/CategoryService.java
+++ src/main/java/com/springboot/blog/service/CategoryService.java
@@ -4,5 +4,6 @@ import com.springboot.blog.payload.CategoryDto;
 
 public interface CategoryService {
     CategoryDto addCategory(CategoryDto categoryDto);
+    CategoryDto getCategory(Long categoryId);
 
 }
